6167e195f6742ae62cec5e0db0ca316bda43f9d5
Shanuka Shehan <57612556+ShanukaOfficial@users.noreply.github.com>
2019-11-12 09:07:47 +0530

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,1 +1,2 @@
 pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+./start-kali.sh
